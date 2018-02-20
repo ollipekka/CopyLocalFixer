19b572a6215dff4f3885609a914172ec631281fc
Olli-Pekka Valtonen <olli-pekka.valtonen@basware.com>
2018-02-20 17:45:56 +0200

    Cleanup and readme update.

--- CopyLocalFixer/Program.fs
+++ CopyLocalFixer/Program.fs
@@ -7,6 +7,54 @@ type ReferenceState =
     | Match of XmlNode
     | NoMatch of XmlNode
 
+let fixCsproj fileName 
+              (outputPathNodes: XmlNodeList) 
+              (doc: XmlDocument) 
+              (nms: XmlNamespaceManager) 
+              verbose =
+
+    let outputPathDebug = outputPathNodes.[0].InnerText
+    let outputPathRelease = outputPathNodes.[1].InnerText
+
+    if outputPathDebug = outputPathRelease then 
+        let references = doc.DocumentElement.SelectNodes ("//foo:Reference", nms)
+        references 
+            |> Seq.cast<XmlNode> 
+            |> Seq.map (fun r ->  
+                let hintPathNode = r.SelectSingleNode ("foo:HintPath", nms)
+                if hintPathNode = null then NoHintPath (r)
+                elif hintPathNode.InnerText.StartsWith outputPathDebug then Match(r)
+                else NoMatch (r))
+            |> Seq.iter (fun referenceState ->
+                match referenceState with
+                | Match(r) ->
+                    let privateNode = r.SelectSingleNode ("foo:Private", nms)
+                    if privateNode = null then
+                        let assemblyNode = r.SelectSingleNode ("@Include", nms)
+                        let node = doc.CreateNode(XmlNodeType.Element, "Private", doc.DocumentElement.NamespaceURI)
+                        node.InnerText <- "True"
+                        r.AppendChild(node) |> ignore
+                        printfn "%s: set Private=True." assemblyNode.Value
+                    else 
+                        privateNode.InnerText <- "True"
+                            
+                        let assemblyNode = r.SelectSingleNode ("@Include", nms)
+                            
+                        printfn "%s: Private was 'False'. Set to 'True'." assemblyNode.Value
+                | NoMatch(r) ->
+                    if verbose then 
+                        let assemblyNode = r.SelectSingleNode ("@Include", nms)
+                        let hintPathNode = r.SelectSingleNode ("foo:HintPath", nms)
+                        printfn "%s: No match for %s." assemblyNode.Value hintPathNode.InnerText
+                | NoHintPath(r) ->
+                    if verbose then
+                        let assemblyNode = r.SelectSingleNode ("@Include", nms)
+                        printfn "%s: No hintpath." assemblyNode.Value
+            )
+    else 
+        printfn "Warning -- %s: Output path %s is diffent from release path %s. Skpping..." fileName outputPathDebug outputPathRelease
+        
+
 [<EntryPoint>]
 let main argv =
     let path = argv.[0]
@@ -23,48 +71,10 @@ let main argv =
         nms.AddNamespace("foo", doc.DocumentElement.NamespaceURI)
         
         let outputPathNodes = doc.DocumentElement.SelectNodes ("//foo:PropertyGroup[@Condition]/foo:OutputPath", nms)
-        let outputPathDebug = outputPathNodes.[0].InnerText
-        let outputPathRelease = outputPathNodes.[1].InnerText
 
-        if outputPathDebug = outputPathRelease then 
-            let references = doc.DocumentElement.SelectNodes ("//foo:Reference", nms)
-            references 
-                |> Seq.cast<XmlNode> 
-                |> Seq.map (fun r ->  
-                    let hintPathNode = r.SelectSingleNode ("foo:HintPath", nms)
-                    if hintPathNode = null then NoHintPath (r)
-                    elif hintPathNode.InnerText.StartsWith outputPathDebug then Match(r)
-                    else NoMatch (r))
-                |> Seq.iter (fun referenceState ->
-                    match referenceState with
-                    | Match(r) ->
-                        let privateNode = r.SelectSingleNode ("foo:Private", nms)
-                        if privateNode = null then
-                            let assemblyNode = r.SelectSingleNode ("@Include", nms)
-                            let node = doc.CreateNode(XmlNodeType.Element, "Private", doc.DocumentElement.NamespaceURI)
-                            node.InnerText <- "True"
-                            r.AppendChild(node) |> ignore
-                            printfn "%s: set Private=True." assemblyNode.Value
-                        else 
-                            privateNode.InnerText <- "True"
-                            
-                            let assemblyNode = r.SelectSingleNode ("@Include", nms)
-                            
-                            printfn "%s: Private was 'False'. Set to 'True'." assemblyNode.Value
-                    | NoMatch(r) ->
-                        if verbose then 
-                            let assemblyNode = r.SelectSingleNode ("@Include", nms)
-                            let hintPathNode = r.SelectSingleNode ("foo:HintPath", nms)
-                            printfn "%s: No match for %s." assemblyNode.Value hintPathNode.InnerText
-                    | NoHintPath(r) ->
-                        if verbose then
-                            let assemblyNode = r.SelectSingleNode ("@Include", nms)
-                            printfn "%s: No hintpath." assemblyNode.Value
-                )
-        else 
-            printfn "Warning -- %s: Output path %s is diffent from release path %s. Skpping..." fileName outputPathDebug outputPathRelease
-        
-        doc.Save(fileName)
+        if outputPathNodes.Count > 0 then
+            fixCsproj fileName outputPathNodes doc nms verbose
+            doc.Save(fileName)
 
 
     )
--- README.md
+++ README.md
@@ -4,6 +4,17 @@
 
 In large builds assemblies are sometimes unneccesary copied. This projects attempts to optimize builds on slow machines by turning on CopyLocal=true conservatively.
 
+It takes reference and checks if it is located in the same directory as the project is built into and changes the reference to "Private".
+
+```xml
+
+    <Reference Include="MyAssembly">
+      <HintPath>outputpath\MyAssembly.dll</HintPath>
+      <Private>True</Private>
+    </Reference>
+
+```
+
 ## Usage
 
 ```bat
